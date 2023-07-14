13f28d07d4e9c998f93ebdb8cb44083d4c0c9e31
Jean-Marc Couffin <jmcouffin@gmail.com>
2023-07-14 09:26:29 +0200

    dz

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,22 @@
-import streamlit_app as st
+import pygwalker as pyg
 import pandas as pd
-import numpy as np
-
-sa = st.StreamlitApp()
-
-st.title('My first app')
+import streamlit.components.v1 as components
+import streamlit as st
+ 
+# Adjust the width of the Streamlit page
+st.set_page_config(
+    page_title="Use Pygwalker In Streamlit",
+    layout="wide"
+)
+ 
+# Add Title
+st.title("Use Pygwalker In Streamlit")
+ 
+# Import your data
+df = pd.read_csv("https://kanaries-app.s3.ap-northeast-1.amazonaws.com/public-datasets/bike_sharing_dc.csv")
+ 
+# Generate the HTML using Pygwalker
+pyg_html = pyg.walk(df, return_html=True)
+ 
+# Embed the HTML into the Streamlit app
+components.html(pyg_html, height=1000, scrolling=True)
